87fd85a77163617c31b9997d5ebbaa723d20648c
Kamil Sałęga <kamilsalega@gmail.com>
2017-08-07 14:31:21 +0200

    Zaczynamy zabawe!!!

---
+++ src/test/java/pl/salega/webszkielet/WebszkieletApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.salega.webszkielet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebszkieletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
