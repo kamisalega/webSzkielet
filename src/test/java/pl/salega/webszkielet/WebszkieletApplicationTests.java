6f70a4f2f4c4b6c156abdaad49e3b9354558d1fa
Kamil Sałęga <kamilsalega@gmail.com>
2017-08-07 19:41:52 +0200

    Dodano funkcje i18n (internacjonalizacja) do webSzkieletu

--- src/test/java/pl/salega/webszkielet/WebszkieletApplicationTests.java
+++ src/test/java/pl/salega/webszkielet/WebszkieletApplicationTests.java
@@ -1,16 +1,29 @@
 package pl.salega.webszkielet;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.salega.webszkielet.web.i18n.I18NService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = WebszkieletApplication.class)
+@WebAppConfiguration
 public class WebszkieletApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+
+    @Autowired
+    private I18NService i18NService;
+
+    @Test
+    public void testMessageByLocaleService() throws Exception {
+		String expectedResults = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+        Assert.assertEquals("The actual and expected String don't match", expectedResults, actual);
+    }
 }
